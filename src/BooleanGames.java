b29f833557b344337b0261071306894721f61c36
1E10 Laptop <sample@bths.edu>
2022-10-24 10:34:50 -0400

    w

--- src/BooleanGames.java
+++ src/BooleanGames.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class BooleanGames {
+public class BooleanGames {
+    private int number;
+
+    public BooleanGames(int number) {
+        this.number = number;
+    }
+
+    public boolean isNegative() {
+        return (number < 0);
+    }
+
+    public boolean exceeds(int num){
+        return (number > num);
+    }
+
+    public boolean isEven() {
+        return (number % 2 == 0);
+    }
+
+    public boolean isOdd() {
+        return (Math.abs(number) % 2 == 1);
+    }
+
+    public boolean isMultipleOf(int x) {
+        return (number % x == 0);
+    }
+
+    public boolean isOnesDigit(int num) {
+        number = Math.abs(number) % 10;
+        return number == num;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        BooleanGames game1 = new BooleanGames(-435);
+        System.out.println(game1.isNegative());
+        System.out.println("----");
+        System.out.println(game1.exceeds(-600));
+        System.out.println(game1.exceeds(-500));
+        System.out.println(game1.exceeds(0));
+        System.out.println("----");
+        System.out.println(game1.isEven());
+        System.out.println(game1.isOdd());
+        System.out.println("----");
+        System.out.println(game1.isMultipleOf(1));
+        System.out.println(game1.isMultipleOf(2));
+        System.out.println(game1.isMultipleOf(3));
+        System.out.println(game1.isMultipleOf(5));
+        System.out.println(game1.isMultipleOf(35));
+        System.out.println(game1.isMultipleOf(145));
+        System.out.println(game1.isMultipleOf(435));
+        System.out.println("----");
+        System.out.println(game1.isOnesDigit(5));
+        System.out.println(game1.isOnesDigit(3));
+        System.out.println(game1.isOnesDigit(4));
+        System.out.println(game1.isOnesDigit(0));
+        System.out.println(game1.isOnesDigit(35));
+
+
+    }
 }
